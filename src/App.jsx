f2bc7ea0526d4df0fbc2a0d8806beae6c15172f8
hine2110 <huyntgde170695@fpt.edu.vn>
2025-09-20 21:15:20 +0700

    template cho benh nhan

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Import pages
+import Home from "./pages/Home";
+// sau này bạn có thể tạo thêm About, Contact... tương tự trong folder pages
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Trang chủ */}
+        <Route path="/" element={<Home />} />
+
+        {/* Ví dụ thêm route khác sau này */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
